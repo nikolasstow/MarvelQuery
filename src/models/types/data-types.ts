95f43d61d381c26ba2ec272c6496d039d4c0fdfa
Nikolas Stow <nikolasstow@gmail.com>
2024-10-03 23:39:00 -0700

    doc tests

--- src/models/types/data-types.ts
+++ src/models/types/data-types.ts
@@ -211,7 +211,7 @@ export type CreatorList = z.infer<typeof CreatorListSchema>;
  * | `available`     | `number`                                | The number of total available characters in this list. Will always be greater than or equal to the "returned" value. |
  * | `returned`      | `number`                                | The number of characters returned in this collection (up to 20). |
  * | `collectionURI` | `string`                                | The path to the full list of characters in this collection.  |
- * | `items`         | [`CharacterSummary`](../data-types.md#charactersummary) | The list of returned characters in this collection.          |
+ * | `items`         | [`Mood`](../configuratioun.md) | The list of returned characters in this collection.          |
  */
 export type CharacterList = z.infer<typeof CharacterListSchema>;
 /**| Property      | Type              | Description                                       |
